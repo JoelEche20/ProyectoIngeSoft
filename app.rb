25d67ae8de806d57ac055ebc19cac2290e01e230
victor torrico sabja <victor.torrico17@gmail.com>
2020-06-15 20:22:27 -0400

    Added attempts to the game

--- app.rb
+++ app.rb
@@ -7,6 +7,7 @@ $quien
 $modo
 $dificultad
 $esMayor=true
+$cont=9
 
 get '/'do 
     erb :Home
@@ -21,6 +22,9 @@ get '/ingresarNombre' do
     $quien=@params[:quien].to_s
     $modo=@params[:modo].to_s
     $dificultad=@params[:dificultad].to_s
+    if @params[:intentos].to_s!=''
+        $cont=@params[:intentos].to_i-1
+    end
     if($quien.to_s=="1 v CPU")
         erb :ingresar_nombre_P2_view
     else
@@ -44,18 +48,29 @@ get '/ingresarIntento' do
 end
 
 post '/respuestaIntento' do
-    cowbull=CowsAndBulls.new
-    if $quien.to_s=="1 v CPU" && $esMayor
-        $esMayor=false
-        $key=cowbull.randomNumber($dificultad)
-    end
-    cowbull.configuration($key.to_s,$modo,$dificultad)
-    @intento=cowbull.playCowsAndBulls(params[:intento].to_s)
-    if(@intento == true)
-        erb :mostrar_mensaje_view
+    if $cont == 0 
+        $cont=9
+        erb :perdiste_view
     else
-        erb :mostrar_mensaje_intento_view
-    end   
+        cowbull=CowsAndBulls.new
+        if $quien.to_s=="1 v CPU" && $esMayor
+            $esMayor=false
+            if $modo=="colores"
+                $key=cowbull.colorsRandom($dificultad)
+            else
+                $key=cowbull.randomNumber($dificultad)
+            end 
+        end
+        cowbull.configuration($key.to_s,$modo,$dificultad)
+        @intento=cowbull.playCowsAndBulls(params[:intento].to_s)
+        if(@intento == true)
+            $cont=9
+            erb :mostrar_mensaje_view
+        else
+            $cont=$cont-1
+            erb :mostrar_mensaje_intento_view
+        end   
+    end
 end
 get '/ingresarNombre2'do
     $nombre= params[:nombre].to_s
